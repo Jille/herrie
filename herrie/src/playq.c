445f72471e17613a53697cc4827dde9f443ee45e
Jille Timmermans <jille@quis.cx>
2012-04-28 16:43:47 +0200

    Add a random space
    as it matches Ed's style.

--- herrie/src/playq.c
+++ herrie/src/playq.c
@@ -258,7 +258,7 @@ playq_init(int autoplay, int xmms, int load_dumpfile)
 		funcs = &xmms_funcs;
 		playq_repeat = 1;
 	}
-	switch(config_getopt_bool("playq.repeat")) {
+	switch (config_getopt_bool("playq.repeat")) {
 		case 1:
 			playq_repeat = 1;
 			break;
